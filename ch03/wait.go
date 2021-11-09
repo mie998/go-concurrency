5ef5d6b78250cbac831ea0b033fa9cf7070c57fe
mie998 <nishiwaki.kyoto@gmail.com>
2021-11-09 23:23:09 +0900

    rwmutex

---
+++ ch03/wait.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"sync"
+)
+
+func ex1() {
+	var wg sync.WaitGroup
+	salutation := "Hello,"
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		fmt.Println(salutation)
+	}()
+	salutation = "hannya???"
+	wg.Wait()
+}
+
+func ex2() {
+	var wg sync.WaitGroup
+	for _, salutation := range []string{"Hello", "hannya???", "unchi"} {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			fmt.Println(salutation)
+		}()
+	}
+	wg.Wait()
+}
+
+func ex3() {
+	var wg sync.WaitGroup
+	for _, salutation := range []string{"Hello", "hannya???", "unchi"} {
+		wg.Add(1)
+		go func(salutation string) {
+			defer wg.Done()
+			fmt.Println(salutation)
+		}(salutation)
+	}
+	wg.Wait()
+}
+
+func ex4() {
+	memConsumed := func() uint64 {
+		runtime.GC()
+		var s runtime.MemStats
+		runtime.ReadMemStats(&s)
+		return s.Sys
+	}
+	var c <-chan interface{}
+	var wg sync.WaitGroup
+	noop := func() { wg.Done(); <-c }
+
+	const numGoroutines = 1e4
+	wg.Add(numGoroutines)
+	before := memConsumed()
+	for i := numGoroutines; i > 0; i-- {
+		go noop()
+	}
+	wg.Wait()
+	after := memConsumed()
+	fmt.Printf("%.3fkb\n", float64(after-before)/numGoroutines/1000)
+}
+
+func main() {
+	ex4()
+}
