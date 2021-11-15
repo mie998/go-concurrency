27632ec32b05130f9ecfef7d281cf72d848d88eb
mie998 <nishiwaki.kyoto@gmail.com>
2021-11-15 13:20:51 +0900

    ch03

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # go-concurrency
 
 Study notes for Concurrency in Go
+
+## メモ
+channel すごくめんごくさそう。特に、channel 記号の <- を右につければいいか左につければいいか曖昧。read, write はどの変数を主体に取るかによって全然意味合いが変わってくるため難しいと思う。
---
+++ ch03/channel_practice.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"os"
+)
+
+func main() {
+	channelWithBuffer()
+}
+
+func deadlock() {
+	stringCh := make(chan string)
+	go func() {
+		for {
+		}
+		stringCh <- "hello"
+	}()
+}
+
+func channelWithBuffer() {
+	var stdoutBuff bytes.Buffer
+	defer stdoutBuff.WriteTo(os.Stdout)
+
+	intCh := make(chan int, 4)
+	go func() {
+		defer close(intCh)
+		defer fmt.Println("Producer done.")
+		for i := 0; i < 5; i++ {
+			fmt.Fprintf(&stdoutBuff, "sending...: %d\n", i)
+			intCh <- i
+		}
+	}()
+
+	for integer := range intCh {
+		fmt.Fprintf(&stdoutBuff, "Recieved: %d\n", integer)
+	}
+}
+
+func channelLifetime() {
+	chanOwner := func() <-chan int {
+		resultCh := make(chan int, 5)
+		go func() {
+			defer close(resultCh)
+			for i := 0; i < 5; i++ {
+				resultCh <- i
+			}
+		}()
+		return resultCh
+	}
+
+	resultCh := chanOwner()
+	for result := range resultCh {
+		fmt.Println("Received:", result)
+	}
+	fmt.Println("Done receiving!")
+}
