d7aa035de9604edd42f57a97fe9743bdac39c9f6
mie998 <nishiwaki.kyoto@gmail.com>
2021-11-25 15:50:16 +0900

    tee

---
+++ ch04/tee.go
@@ -0,0 +1,44 @@
+package main
+
+func orDone(done, c <-chan interface{}) <-chan interface{} {
+	valStream := make(chan interface{})
+	go func() {
+		defer close(valStream)
+		for {
+			select {
+			case <-done:
+				return
+			case val, ok := <-c:
+				if !ok {
+					return
+				}
+				select {
+				case valStream <- val:
+				case <-done:
+				}
+			}
+		}
+	}()
+	return valStream
+}
+
+func tee(done <-chan interface{}, in <-chan interface{}) (_, _ <-chan interface{}) {
+	out1 := make(chan interface{})
+	out2 := make(chan interface{})
+	go func() {
+		defer close(out1)
+		defer close(out2)
+		for val := range orDone(done, in) {
+			var out1, out2 = out1, out2
+			for i := 0; i < 2; i++ {
+				select {
+				case out1 <- val:
+					out1 = nil
+				case out2 <- val:
+					out2 = nil
+				}
+			}
+		}
+	}()
+	return out1, out2
+}
