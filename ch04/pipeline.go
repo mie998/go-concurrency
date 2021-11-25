0a31e8e9fcca52c2208fe487698b0fb2a6c1816f
mie998 <nishiwaki.kyoto@gmail.com>
2021-11-25 12:20:42 +0900

    fanIn

--- ch04/pipeline.go
+++ ch04/pipeline.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"sync"
 )
 
 func main() {
@@ -127,4 +128,32 @@ func diceRoll() {
 	for num := range take(done, repeatFn(done, rand), 10) {
 		fmt.Println(num)
 	}
+}
+
+func fanIn(done <-chan interface{}, channels ...<-chan interface{}) <-chan interface{} {
+	var wg sync.WaitGroup
+	multiplexedStream := make(chan interface{})
+
+	multiplex := func(c <-chan interface{}) {
+		defer wg.Done()
+		for i := range c {
+			select {
+			case <-done:
+				return
+			case multiplexedStream <- i:
+			}
+		}
+	}
+
+	wg.Add(len(channels))
+	for _, c := range channels {
+		go multiplex(c)
+	}
+
+	go func() {
+		wg.Wait()
+		close(multiplexedStream)
+	}()
+
+	return multiplexedStream
 }
